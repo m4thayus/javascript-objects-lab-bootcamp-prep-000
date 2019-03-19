0fe28764f4de340924f3cf093dcce617446288f4
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 17:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object, );
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
